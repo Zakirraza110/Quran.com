5f89c26679b9e65a6be6b866c3e144e95ce58578
Zakir <zaakiraza110@gmail.com>
2024-04-06 19:41:19 -0700

    update footer

--- file.js
+++ file.js
@@ -1,6 +1,6 @@
 async function logMovies() {
     let id, chapter;
-    for (let i = 1; i <= 12; i++) {
+    for (let i = 1; i <= 5; i++) {
         const response = await fetch(`https://api.quran.com/api/v4/chapter_recitations/${i}/${i}`);
         const movies = await response.json();
         link = movies?.audio_file?.audio_url;

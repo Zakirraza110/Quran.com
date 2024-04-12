d4bff6e5247dc73c6f59199e1122c189f00c4a3e
Zakir <zaakiraza110@gmail.com>
2024-04-12 04:23:40 -0700

    audio statrt

--- file.js
+++ file.js
@@ -2,7 +2,7 @@
 
 async function logMovies() {
     let id, chapter;
-    for (let i = 1; i <= 5; i++) {
+    for (let i = 1; i <=114; i++) {
         const response = await fetch(`https://api.quran.com/api/v4/chapter_recitations/${i}/${i}`);
         const movies = await response.json();
         link = movies?.audio_file?.audio_url;
@@ -24,3 +24,7 @@ function fun1(){
     header.style.display="block";
     nav_manu.style.display="none";
 }
+
+
+// Audio
+
